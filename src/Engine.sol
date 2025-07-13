df1d7cd40018d2a0e8c96633aa020166d9ac469d
nouman <noumankhan95@yahoo.com>
2025-07-13 19:37:19 +0500

    helper functions tbd

--- src/Engine.sol
+++ src/Engine.sol
@@ -5,10 +5,12 @@ import {DSCToken} from "./DSCToken.sol";
 contract PoolEngine {
     //Errors
     error PoolEngine__PriceFeedAndTokenLengthNotEqual();
-
+    error PoolEngine__AmountMustbeMoreThanZero();
+    error PoolEngine__CollateralTypeNotAllowed();
     //   Storage Values       //
     address[] public s_approvedTokens;
     mapping(address => address) s_tokenToPriceFeed;
+    mapping(address => mapping(address => uint256)) s_userToCollateral;
 
     //Constructor Values //
     constructor(
@@ -25,11 +27,34 @@ contract PoolEngine {
         }
     }
 
-    function depositCollateral() external {}
+    // Modifiers
+
+    modifier isMoreThanZero(uint256 _amount) {
+        require(_amount > 0, PoolEngine__AmountMustbeMoreThanZero());
+        _;
+    }
+    modifier isTokenAllowed(address _tokenAddress) {
+        require(
+            s_tokenToPriceFeed[_tokenAddress] != address(0),
+            PoolEngine__CollateralTypeNotAllowed()
+        );
+        _;
+    }
+
+    //Main Functionality
+    function depositCollateral(
+        uint256 _amount,
+        address _collateralAddress
+    ) external isMoreThanZero(_amount) isTokenAllowed(_collateralAddress) {
+        s_userToCollateral[msg.sender][_collateralAddress] += _amount;
+    }
 
     function mintDSC() external {}
 
     function redeemCollateral() external {}
 
     function liquidate() external {}
+
+
+    //Helper Functions
 }
