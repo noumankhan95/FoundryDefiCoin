0167c4ab9045d3632834691041899995b75c3cb3
nouman <noumankhan95@yahoo.com>
2025-07-14 00:21:18 +0500

    adding the mint and deposit functionalities

--- src/DSCToken.sol
+++ src/DSCToken.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.18;
 import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract DefiToken is ERC20, Ownable {
+contract DSCToken is ERC20, Ownable {
     constructor() ERC20("LIFI Token", "LF") Ownable(msg.sender) {}
 
-    function mintToken(uint256 _amount) external {
-        _mint(msg.sender, _amount);
+    function mintToken(address account,uint256 _amount) external {
+        _mint(account, _amount);
     }
 
     function burnTokens(address _account, uint256 _amount) external {
--- src/Engine.sol
+++ src/Engine.sol
@@ -1,21 +1,38 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 import {DSCToken} from "./DSCToken.sol";
+import {AggregatorV3Interface} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {IERC20} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 
 contract PoolEngine {
     //Errors
     error PoolEngine__PriceFeedAndTokenLengthNotEqual();
     error PoolEngine__AmountMustbeMoreThanZero();
     error PoolEngine__CollateralTypeNotAllowed();
+    error PoolEngine__CollateralTransferFailed();
     //   Storage Values       //
     address[] public s_approvedTokens;
     mapping(address => address) s_tokenToPriceFeed;
     mapping(address => mapping(address => uint256)) s_userToCollateral;
+    mapping(address => uint256) s_usertomintedDSC;
+    uint256 internal constant MULTIPLICATION_PRECISION = 1e10;
+    uint256 internal constant DIVISION_PRECISION = 1e18;
+    uint256 internal constant LIQUIDATION_THRESHOLD = 50;
+    uint256 internal constant ADDITIONAL_DIVISION_PRECISION = 100;
+    DSCToken internal s_dscToken;
+    // Events
+
+    event DSCToken_minted(address indexed user);
+    event DSCToken_collateralDeposited(
+        address indexed user,
+        uint256 indexed amount
+    );
 
     //Constructor Values //
     constructor(
         address[] memory _approvedTokens,
-        address[] memory _tokenPriceFeeds
+        address[] memory _tokenPriceFeeds,
+        address dSCTokenAddress
     ) {
         require(
             _approvedTokens.length == _tokenPriceFeeds.length,
@@ -25,6 +42,7 @@ contract PoolEngine {
             s_tokenToPriceFeed[_approvedTokens[i]] = _tokenPriceFeeds[i];
             s_approvedTokens.push(_approvedTokens[i]);
         }
+        s_dscToken = DSCToken(dSCTokenAddress);
     }
 
     // Modifiers
@@ -42,19 +60,70 @@ contract PoolEngine {
     }
 
     //Main Functionality
-    function depositCollateral(
+    function depositCollateralAndMintDSC(
         uint256 _amount,
         address _collateralAddress
     ) external isMoreThanZero(_amount) isTokenAllowed(_collateralAddress) {
-        s_userToCollateral[msg.sender][_collateralAddress] += _amount;
+        depositCollateral(_amount, _collateralAddress);
+        mintDSC(_amount, _collateralAddress);
     }
 
-    function mintDSC() external {}
+    function depositCollateral(
+        uint256 _amount,
+        address _collateralAddress
+    ) internal {
+        s_userToCollateral[msg.sender][_collateralAddress] += _amount;
+        uint256 collateralInUsd = getCollateralAmountInUsd(
+            _collateralAddress,
+            _amount
+        );
+        emit DSCToken_collateralDeposited(msg.sender, _amount);
+    }
+
+    function mintDSC(
+        uint256 _amount,
+        address _collateral
+    ) internal isMoreThanZero(_amount) {
+        s_usertomintedDSC[msg.sender] += _amount;
+        bool success = IERC20(_collateral).transferFrom(
+            msg.sender,
+            address(this),
+            _amount
+        );
+        s_dscToken.mintToken(msg.sender, _amount);
+        emit DSCToken_minted(msg.sender);
+        if (!success) {
+            revert PoolEngine__CollateralTransferFailed();
+        }
+    }
 
     function redeemCollateral() external {}
 
     function liquidate() external {}
 
-
     //Helper Functions
+
+    function getCollateralAmountInUsd(
+        address _collateralTypeAddress,
+        uint256 _amount
+    )
+        internal
+        view
+        isTokenAllowed(_collateralTypeAddress)
+        isMoreThanZero(_amount)
+        returns (uint256)
+    {
+        (, int256 price, , , ) = AggregatorV3Interface(_collateralTypeAddress)
+            .latestRoundData();
+        return
+            ((uint256(price) * MULTIPLICATION_PRECISION) * _amount) /
+            DIVISION_PRECISION;
+    }
+
+    function calculateAmountToLend(
+        uint256 _amount
+    ) internal pure returns (uint256) {
+        return
+            (_amount * LIQUIDATION_THRESHOLD) / ADDITIONAL_DIVISION_PRECISION;
+    }
 }
